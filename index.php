607aba49dcab7c884e2cf9444d6d4e31100156ca
Nikez44 <nike-o_94@hotmail.com>
2015-06-06 11:35:25 -0500

    agregado el div content para centrar el contenido de la informacion

--- index.php
+++ index.php
@@ -9,52 +9,54 @@
 
     <?php include 'partials/_header.html' ?>
 
-    <header id="index_header">
-        <h1 id="index_title">Post Recientes</h1>
+    <div id="content">
+        <header id="index_header">
+            <h1 id="index_title">Post Recientes</h1>
 
-        <div id="search_input">
-            Buscar <input type="text"/>
-        </div>
+            <div id="search_input">
+                Buscar <input type="text"/>
+            </div>
 
-        <div class='clear'></div>
-    </header>
+            <div class='clear'></div>
+        </header>
 
-    <aside class="left">
-        <section>
-            <h2>Entradas</h2>
+        <aside class="left">
+            <section>
+                <h2>Entradas</h2>
+            </section>
+        </aside>
+
+        <section class="center">
+            <h2>News Section</h2>
+
+            <article>
+                <h2>News Article</h2>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+            </article>
+
+            <article>
+                <h2>News Article</h2>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+                <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
+                    lurum hurum turum.</p>
+            </article>
         </section>
-    </aside>
 
-    <section class="center">
-        <h2>News Section</h2>
-
-        <article>
-            <h2>News Article</h2>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-        </article>
-
-        <article>
-            <h2>News Article</h2>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-            <p>Ipsum lurum hurum turum ipsum lurum hurum turum ipsum lurum hurum turum ipsum
-                lurum hurum turum.</p>
-        </article>
-    </section>
-
-    <aside class="right">
-        <section>
-            <h2>Noticias</h2>
-        </section>
-    </aside>
-
+        <aside class="right">
+            <section>
+                <h2>Noticias</h2>
+            </section>
+        </aside>
+    </div>
+    <div class='clear'></div>
    <?php include 'partials/_footer.html' ?>
 
 </body>
