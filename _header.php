5813744db57ca2dd51d8628535dd4060b0e2d2c8
Oscar <nike-o_94@hotmail.com>
2015-06-26 14:24:30 -0500

    agregago un php

---
+++ _header.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oscar
+ * Date: 26/06/15
+ * Time: 02:23 PM
+ */
+?>
+
+<header class="header">
+    <div class="container">
+        <h1 class="header-heading"><img class="img-responsive" src="img/unblogIcono.png">UnBlog</h1>
+    </div>
+</header>
+
+<nav class="nav-bar">
+    <div class="container">
+        <ul class="nav">
+            <li><a href="index.html">Victor Sosa</a></li>
+            <li><a href="about.html"><u>Sobre Mí</u></a></li>
+            <li><a href="contacto.html">Contacto</a></li>
+            <li><a href="#">Cerrar Sesión</a></li>
+        </ul>
+    </div>
+</nav>
